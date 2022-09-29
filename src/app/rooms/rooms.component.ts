1757b2b72b0eaf995c8f8f7034242aba8e117a54
ilkerserce <ilker@serceajans.com>
2022-09-29 18:40:44 +0300

    Save

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -19,78 +19,51 @@ export class RoomsComponent implements OnInit {
     bookedRooms: 5,
   }
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: "Deluxe Room",
-      amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
-      price: 500,
-      photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutDate: new Date('12-Nov-2021'),
-      rating: "3.4",
-    },
-    {
-      roomNumber: 2,
-      roomType: "Deluxe Room",
-      amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
-      price: 1000,
-      photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutDate: new Date('12-Nov-2021'),
-      rating: "3.9",
-    },
-    {
-      roomNumber: 3,
-      roomType: "Private Suite",
-      amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
-      price: 15000,
-      photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutDate: new Date('12-Nov-2021'),
-      rating: "4.5",
-    }
-  ];
+  roomList: RoomList[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-    // this.roomList = [
-    //   {
-    //     roomNumber: 1,
-    //     roomType: "Deluxe Room",
-    //     amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
-    //     price: 500,
-    //     photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
-    //     checkinTime: new Date('11-Nov-2021'),
-    //     checkoutDate: new Date('12-Nov-2021'),
-    //     rating: "3.4",
-    //   },
-    //   {
-    //     roomNumber: 2,
-    //     roomType: "Deluxe Room",
-    //     amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
-    //     price: 1000,
-    //     photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
-    //     checkinTime: new Date('11-Nov-2021'),
-    //     checkoutDate: new Date('12-Nov-2021'),
-    //     rating: "3.923232",
-    //   },
-    //   {
-    //     roomNumber: 3,
-    //     roomType: "Private Suite",
-    //     amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
-    //     price: 15000,
-    //     photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
-    //     checkinTime: new Date('11-Nov-2021'),
-    //     checkoutDate: new Date('12-Nov-2021'),
-    //     rating: "4.5",
-    //   },
-    // ];
+    this.roomList = [
+      {
+        roomNumber: 1,
+        roomType: "Deluxe Room",
+        amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
+        price: 500,
+        photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutDate: new Date('12-Nov-2021'),
+        rating: "3.4",
+      },
+      {
+        roomNumber: 2,
+        roomType: "Deluxe Room",
+        amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
+        price: 1000,
+        photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutDate: new Date('12-Nov-2021'),
+        rating: "3.923232",
+      },
+      {
+        roomNumber: 3,
+        roomType: "Private Suite",
+        amenities: "Air Condition, Free Wifi, TV, Bathroom, Kitchen",
+        price: 15000,
+        photos: 'https://image.unsplash.com/photo-1518791841217-8f162fle1131',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutDate: new Date('12-Nov-2021'),
+        rating: "4.5",
+      },
+    ];
   }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(room: RoomList) {
+    console.log(room);
   }
 
 }
